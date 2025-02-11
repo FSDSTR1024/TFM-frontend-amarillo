d926bb589fa22492f6172cb6d270e54a885cadb1
Oriana Pinto <orianapintosilveira@gmail.com>
2025-02-11 13:13:52 +0100

    mostrar whizzes en feed

---
+++ tfm-frontend/src/components/whizzesCard/WhizzesCard.jsx
@@ -0,0 +1,44 @@
+import like from "../../assets/icons/like.svg";
+import comment from "../../assets/icons/comentario-alt.svg";
+import reWhizz from "../../assets/icons/retuit-de-flechas.svg";
+import send from "../../assets/icons/send.svg";
+import "./WhizzesCard.css";
+
+export const WhizzesCard = ({ whizz }) => {
+  return (
+    <div className="whizz-card">
+      <h4>@{whizz.username}</h4>
+      <p>{whizz.content}</p>
+
+      {whizz.media && whizz.media.length > 0 && (
+        <div className="whizz-card-media">
+          {whizz.media.map((url, index) =>
+            url.includes("image") ? (
+              <img
+                key={index}
+                src={url}
+                alt="whizzes media"
+                className="whizz-image"
+              />
+            ) : (
+              <video
+                key={index}
+                src={url}
+                alt="whizzes media"
+                className="whizz-video"
+                controls
+              />
+            )
+          )}
+        </div>
+      )}
+
+      <div className="whizz-card-icons">
+        <img className="like" src={like} alt="like" />
+        <img src={comment} alt="comment" />
+        <img className="re-whizz" src={reWhizz} alt="reWhizz" />
+        <img src={send} alt="send" />
+      </div>
+    </div>
+  );
+};
--- tfm-frontend/src/components/whizzesContainer/WhizzesContainer.jsx
+++ tfm-frontend/src/components/whizzesContainer/WhizzesContainer.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import { WhizzesCard } from "../whizzesCard/WhizzesCard";
+import "./WhizzesContainer.css";
+
 const WhizzesContainer = () => {
+  const [whizzes, setWhizzes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/whizzes")
+      .then((response) => response.json())
+      .then((data) => {
+        setWhizzes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al cargar whizzes", error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="whizzes-container">
-      <h1>Whizzes</h1>
-      <h1>Whizzes</h1>
-      <h1>Whizzes</h1>
+      {loading ? (
+        <p>Cargando whizzes...</p>
+      ) : (
+        whizzes.map((whizz) => <WhizzesCard key={whizz._id} whizz={whizz} />)
+      )}
     </div>
   );
 };
