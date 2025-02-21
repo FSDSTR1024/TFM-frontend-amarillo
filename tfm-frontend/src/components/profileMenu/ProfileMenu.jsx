71e05a4c08d55ae7baff05018e64259f1d34a487
Oriana Pinto <orianapintosilveira@gmail.com>
2025-02-21 18:53:17 +0100

    cloudinary profile

--- tfm-frontend/src/components/profileMenu/ProfileMenu.jsx
+++ tfm-frontend/src/components/profileMenu/ProfileMenu.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
-import perfil from '../../assets/icons/perfil.svg'
-import logo from '../../assets/icons/whiz.svg'
-import './ProfileMenu.css';
+import { useEffect, useState } from "react";
+import perfil from "../../assets/icons/perfil.svg";
+import logo from "../../assets/icons/whiz.svg";
+import "./ProfileMenu.css";
 
-const cloudinary_url = 'https://api.cloudinary.com/v1_1';
+const cloudinary_url = "https://api.cloudinary.com/v1_1";
 const cloudinary_upload_preset = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
 const cloudinary_cloud_name = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 
 const ProfileMenu = () => {
-  const userId = localStorage.getItem('userId');
+  const userId = localStorage.getItem("userId");
   console.log("El id de usuario es:", userId);
 
   const [userData, setUserData] = useState({});
@@ -30,30 +30,45 @@ const ProfileMenu = () => {
   }
 
   return (
-    <div className='profile-container'>
-      <div className='profile-banner'>
-        <img className='banner' src={logo} alt='logo' />
+    <div className="profile-container">
+      <div className="profile-banner">
+        <img className="banner" src={logo} alt="logo" />
       </div>
-      <img className='avatar' src={perfil} alt='avatar' />
+      <img className="avatar" src={perfil} alt="avatar" />
 
-      <div className='profile-details'>
-        <h2 className='profile-name'>{userData.name} {userData.surname}</h2>
-        <p className='profile-username'>@{userData.username}</p>
-        <p className='profile-location'> Actualizar modelo para location</p>
-        <p className='profile-bio'> Actualizar modelo para fecha de nacimiento</p>
-        <p className='profile-joined'> Se unió en {new Date(userData.createdAt).toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+      <div className="profile-details">
+        <h2 className="profile-name">
+          {userData.name} {userData.surname}
+        </h2>
+        <p className="profile-username">@{userData.username}</p>
+        <p className="profile-location"> Actualizar modelo para location</p>
+        <p className="profile-bio">
+          {" "}
+          Actualizar modelo para fecha de nacimiento
+        </p>
+        <p className="profile-joined">
+          {" "}
+          Se unió en{" "}
+          {new Date(userData.createdAt).toLocaleDateString("es-ES", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
       </div>
 
-      <div className='profile-stats'>
-        <span><strong>{userData.followingCount}</strong> siguiendo</span>
-        <span><strong>{userData.followersCount}</strong> seguidores</span>
+      <div className="profile-stats">
+        <span>
+          <strong>{userData.followingCount}</strong> siguiendo
+        </span>
+        <span>
+          <strong>{userData.followersCount}</strong> seguidores
+        </span>
       </div>
 
-      <button className='edit-profile-btn'>Editar perfil</button>
+      <button className="edit-profile-btn">Editar perfil</button>
     </div>
   );
 };
 
 export default ProfileMenu;
-
-export default ProfileMenu
