5822fa3b1684ca3e7deed67180b5c357cca27534
hdulon92 <84352890+H3Nutcracker@users.noreply.github.com>
2025-02-26 12:54:00 +0100

    Logica de subida de archivos migrada al backend y .env eliminado del repo

--- tfm-frontend/src/components/whizzesForm/WhizzesForm.jsx
+++ tfm-frontend/src/components/whizzesForm/WhizzesForm.jsx
@@ -5,10 +5,6 @@ import video from '../../assets/icons/video camara.svg';
 import crear from '../../assets/icons/derecho.svg'
 import './WhizzesForm.css';
 
-const cloudinary_url = 'https://api.cloudinary.com/v1_1';
-const cloudinary_upload_preset = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
-const cloudinary_cloud_name = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
-
 const CreateWhizz = () => {
   const [content, setContent] = useState('');
   const [media, setMedia] = useState([]);
@@ -19,30 +15,24 @@ const CreateWhizz = () => {
     document.querySelector('.'+target).click();
   }
 
-  const getMediaTypes = (type) => {
-    if (type.includes('image')) return 'image';
-    if (type.includes('video')) return 'video';
-    return 'raw';
-  };
-
   const handleFileUpload = async (file) => {
     if (!file) return;
 
-    const mediaType = getMediaTypes(file.type);
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', cloudinary_upload_preset);
 
     try {
-      const response = await fetch(`${cloudinary_url}/${cloudinary_cloud_name}/${mediaType}/upload`, {
+      const response = await fetch(`http://localhost:3000/upload`, {
         method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
         body: formData,
       });
-
       const data = await response.json();
 
-      if (data.secure_url) {
-        setMedia([...media, data.secure_url]);
+      if (data.url) {
+        setMedia((prevMedia) => [...prevMedia, data.url]);
       }
     } catch (error) {
       console.error('Error subiendo el archivo:', error);
