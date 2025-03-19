988564afb7a57bc010a686c0d5ff22706a01254e
Oriana <orianapintosilveira@gmail.com>
2025-03-19 12:32:29 +0100

    ADD Profile Claudinary

--- tfm-frontend/src/components/menuBar/MenuBar.jsx
+++ tfm-frontend/src/components/menuBar/MenuBar.jsx
@@ -1,24 +1,29 @@
-import { Link } from 'react-router-dom'
-import './MenuBar.css'
-import inicio from '../../assets/icons/home.svg'
-import lupa from '../../assets/icons/lupa.svg'
-import mensajes from '../../assets/icons/sobre.svg'
-import notificaciones from '../../assets/icons/campana.svg'
-import crear from '../../assets/icons/add.svg'
+import { Link } from "react-router-dom";
+import "./MenuBar.css";
+import inicio from "../../assets/icons/home.svg";
+import lupa from "../../assets/icons/lupa.svg";
+import mensajes from "../../assets/icons/sobre.svg";
+import notificaciones from "../../assets/icons/campana.svg";
+import crear from "../../assets/icons/add.svg";
 
 const MenuBar = () => {
-
   return (
     <div className="menu-bar-container">
       <nav className="menu-bar">
-        <Link to="/main"><img src={inicio} /></Link>
-        <Link to="/search"><img src={lupa} /></Link>
-        <Link to="/whizzes" className='add'><img src={crear} /></Link>
-        <Link to="/messages"><img src={mensajes} /></Link>
-        <Link to="/notifications"><img src={notificaciones} /></Link>
+        <Link to="/main">
+          <img src={inicio} />
+        </Link>
+        <Link to="/search">
+          <img src={lupa} />
+        </Link>
+        <Link to="/whizzes" className="add">
+          <img src={crear} />
+        </Link>
+        <img src={mensajes} />
+        <img src={notificaciones} />
       </nav>
     </div>
   );
-}
+};
 
 export default MenuBar;
--- tfm-frontend/src/components/profileMenu/ProfileMenu.jsx
+++ tfm-frontend/src/components/profileMenu/ProfileMenu.jsx
@@ -14,6 +14,7 @@ const ProfileMenu = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [userWhizzes, setUserWhizzes] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const openModal = () => {
@@ -40,7 +41,22 @@ const ProfileMenu = () => {
       .catch((error) => {
         console.error("Error al cargar whizzes", error);
       });
-  },);
+  });
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`${backendUrl}/users/${userId}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        });
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error al cargar el perfil", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
   if (isLoading) {
     return <p>Cargando perfil...</p>;
@@ -50,66 +66,136 @@ const ProfileMenu = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userId");
     navigate("/");
-  }
+  };
 
+  const ProfilePicUpload = async (file, type) => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("type", type);
+
+    try {
+      const response = await fetch(`${backendUrl}/upload`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: formData,
+      });
+      const data = await response.json();
+
+      if (data.url) {
+        const updateResponse = await fetch(
+          `${backendUrl}/users/update-profile`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+            body: JSON.stringify({ [type]: data.url }),
+          }
+        );
+
+        const updatedUser = await updateResponse.json();
+        setUserData((prevUserData) => ({ ...prevUserData, ...updatedUser }));
+      }
+    } catch (error) {
+      console.error("Error subiendo la imagen:", error);
+      alert("Error subiendo al subir la imagen de perfil");
+    }
+  };
 
   return (
     <>
-    <Modal
-      isOpen={isModalOpen}
-      onClose={() => setIsModalOpen(false)}
-      onConfirm={handleLogout}
-      title="Cerrar sesión"
-      message="¿Seguro que quieres cerrar sesión?"
-      confirmText="Cerrar sesión"
-      cancelText="Cancelar"
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleLogout}
+        title="Cerrar sesión"
+        message="¿Seguro que quieres cerrar sesión?"
+        confirmText="Cerrar sesión"
+        cancelText="Cancelar"
       />
 
-    <div className="profile-container">
-      <div className="profile-banner">
-        <img className="banner" src={logo} alt="logo" />
-      </div>
-      <img className="avatar" src={perfil} alt="avatar" />
+      <div className="profile-container">
+        <div className="profile-banner">
+          <label>
+            <img
+              className="banner"
+              src={userData.bannerImage || logo}
+              alt="banner"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={(e) =>
+                ProfilePicUpload(e.target.files[0], "bannerImage")
+              }
+            />
+          </label>
+        </div>
+        <label className="profile-pic-container">
+          <img
+            className="profile-picture"
+            src={userData.profilePicture || perfil}
+            alt="picture"
+          />
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={(e) =>
+              ProfilePicUpload(e.target.files[0], "profilePicture")
+            }
+          />
+        </label>
+        <div className="profile-details">
+          <h2 className="profile-name">
+            {userData.name} {userData.surname}
+          </h2>
+          <p className="profile-username">@{userData.username}</p>
+          <p className="profile-location">{userData.location}</p>
+          <p className="profile-birthdate">
+            <img className="balloon" src={globo} alt="globo" />{" "}
+            {userData.birthdate}
+          </p>
+          <p className="profile-joined">
+            {" "}
+            Se unió en{" "}
+            {new Date(userData.createdAt).toLocaleDateString("es-ES", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+        </div>
 
-      <div className="profile-details">
-        <h2 className="profile-name">
-          {userData.name} {userData.surname}
-        </h2>
-        <p className="profile-username">@{userData.username}</p>
-        <p className="profile-location">{userData.location}</p>
-        <p className="profile-birthdate"><img className="balloon" src={globo} alt="globo" /> {userData.birthdate}</p>
-        <p className="profile-joined">
-          {" "}
-          Se unió en{" "}
-          {new Date(userData.createdAt).toLocaleDateString("es-ES", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </p>
-      </div>
+        <div className="profile-stats">
+          <span>
+            <strong>{userData.followingCount}</strong> siguiendo
+          </span>
+          <span>
+            <strong>{userData.followersCount}</strong> seguidores
+          </span>
+        </div>
 
-      <div className="profile-stats">
-        <span>
-          <strong>{userData.followingCount}</strong> siguiendo
-        </span>
-        <span>
-          <strong>{userData.followersCount}</strong> seguidores
-        </span>
+        <button className="logout-btn" onClick={openModal}>
+          Cerrar sesión
+        </button>
       </div>
-
-      <button className="logout-btn" onClick={openModal}>Cerrar sesión</button>
-    </div>
-    <div className="profile-whizzes">
-    {isLoading ? (
-      <p>Cargando whizzes...</p>
-    ) : (
-      userWhizzes.map((whizz) => (
-        <WhizzesCard key={whizz._id} whizz={whizz} />
-      ))
-    )}
-  </div>
-  </>
+      <div className="profile-whizzes">
+        {isLoading ? (
+          <p>Cargando whizzes...</p>
+        ) : (
+          userWhizzes.map((whizz) => (
+            <WhizzesCard key={whizz._id} whizz={whizz} />
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
